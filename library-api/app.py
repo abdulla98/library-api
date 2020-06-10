c8767e6a177ed3f79624b9a0103c358a7567698f
Abdulla Bakija <abdulla.bakija@inspire11.com>
2020-06-10 16:10:05 +0200

    Update app routes

--- library-api/app.py
+++ library-api/app.py
@@ -11,65 +11,65 @@ def welcome():
 
 
 # Student endpoints
-@app.route('/createStudent', methods=['POST'])
+@app.route('/student/save', methods=['POST'])
 def add_student():
     create_student(request.json['name'], request.json['birthdate'], request.json['gender'],
                    request.json['student_class'])
     return 'Student is created'
 
 
-@app.route('/allStudents', methods=['GET'])
+@app.route('/students', methods=['GET'])
 def get_all_students():
     return get_student(None)
 
 
-@app.route('/getStudentById/<int:student_id>', methods=['GET'])
+@app.route('/student/<int:student_id>', methods=['GET'])
 def get_student_by_id(student_id):
     return get_student(student_id)
 
 
-@app.route('/updateStudent/<int:student_id>', methods=['PUT'])
+@app.route('/student/update/<int:student_id>', methods=['PUT'])
 def update_student(student_id):
     update_student(student_id, request.json['name'], request.json['birthdate'], request.json['gender'],
                    request.json['student_class'])
     return 'Student is updated!'
 
 
-@app.route('/deleteStudent/<int:student_id>', methods=['DELETE'])
+@app.route('/student/delete/<int:student_id>', methods=['DELETE'])
 def delete_student(student_id):
     return delete_student(student_id)
 
 
 # Author endpoints
-@app.route('/createAuthor', methods=['POST'])
+@app.route('/author/save', methods=['POST'])
 def add_author():
     create_author(request.json['name'])
     return 'Author is created'
 
 
-@app.route('/allAuthors', methods=['GET'])
+@app.route('/authors', methods=['GET'])
 def get_all_authors():
     return get_author(None)
 
 
-@app.route('/getAuthorById/<int:author_id>', methods=['GET'])
+@app.route('/author/<int:author_id>', methods=['GET'])
 def get_author_by_id(author_id):
     return get_author(author_id)
 
 
-@app.route('/updateAuthor/<int:author_id>', methods=['PUT'])
+@app.route('/author/update/<int:author_id>', methods=['PUT'])
 def update_author(author_id):
     update_author(author_id, request.json['name'])
     return 'Author is updated!'
 
 
-@app.route('/deleteAuthor/<int:author_id>', methods=['DELETE'])
+@app.route('/author/delete/<int:author_id>', methods=['DELETE'])
 def delete_author(author_id):
     return delete_author(author_id)
 
 
 # Book endpoints
-@app.route('/createBook', methods=['POST'])
+@app.route('/book/save', methods=['POST'])
 def add_book():
     author = Author.query.get(request.json['author_id'])
     if author:
@@ -79,29 +79,29 @@ def add_book():
         return 'This author does not exist!'
 
 
-@app.route('/allBooks', methods=['GET'])
+@app.route('/books', methods=['GET'])
 def get_all_books():
     return get_book(None)
 
 
-@app.route('/getBookById/<int:book_id>', methods=['GET'])
+@app.route('/book/<int:book_id>', methods=['GET'])
 def get_book_by_id(book_id):
     return get_book(book_id)
 
 
-@app.route('/updateBook/<int:book_id>', methods=['PUT'])
+@app.route('/book/update/<int:book_id>', methods=['PUT'])
 def update_book(book_id):
     update_book(book_id, request.json['name'], request.json['pagecount'])
     return 'Book is updated successfully!'
 
 
-@app.route('/deleteBook/<int:book_id>', methods=['DELETE'])
+@app.route('/book/delete/<int:book_id>', methods=['DELETE'])
 def delete_book(book_id):
     return delete_book(book_id)
 
 
 # Borrow endpoints
-@app.route('/createBorrow', methods=['POST'])
+@app.route('/borrow/save', methods=['POST'])
 def add_borrow():
     student = Student.query.get(request.json['student_id'])
     book = Book.query.get(request.json['book_id'])
@@ -114,22 +114,22 @@ def add_borrow():
     else:
         return 'This book does not exist!'
 
-@app.route('/allBorrows', methods=['GET'])
+@app.route('/borrows', methods=['GET'])
 def get_all_borrows():
     return get_borrow(None)
 
-@app.route('/getBorrowById/<int:borrow_id>', methods=['GET'])
+@app.route('/borrow/<int:borrow_id>', methods=['GET'])
 def get_borrow_by_id(borrow_id):
     return get_borrow(borrow_id)
 
 
-@app.route('/updateBorrow/<int:borrow_id>', methods=['PUT'])
+@app.route('/borrow/update/<int:borrow_id>', methods=['PUT'])
 def update_borrow(borrow_id):
     update_borrow(borrow_id, request.json['taken_date'], request.json['brought_date'])
     return 'Borrow is updated successfully!'
 
 
-@app.route('/deleteBorrow/<int:borrow_id>', methods=['DELETE'])
+@app.route('/borrow/delete/<int:borrow_id>', methods=['DELETE'])
 def delete_borrow(borrow_id):
     return delete_borrow(borrow_id)
 
